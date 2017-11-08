729a4df1c516ad8f8d6a332cd36b03040ed1bdbf
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 05:38:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+
 end
 
 
@@ -16,10 +16,6 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
   shipping_manifest["pearl necklace"] = 1
-
+  shipping_manifest
 end
-
-
-
