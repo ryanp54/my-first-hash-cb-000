6da868b2b48023cec0b6c205b1b176f7cc9f6de4
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 05:37:57 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,24 +6,18 @@ end
 
 
 def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-  #your code here
-  #remember to return the shipping_manifest hash
+  shipping_manifest["pearl necklace"] = 1
 
 end
 
